8b1c12ffa9208feca670294135de9543290ce177
Arreche Cristian <arrechecris@gmail.com>
2018-11-09 16:19:38 -0300

    Baja de residencia

--- HSH/app/controllers/residences_controller.rb
+++ HSH/app/controllers/residences_controller.rb
@@ -17,20 +17,29 @@ class ResidencesController < ApplicationController
   end
 
   def destroy
+  	#Recibo como parametro el id de la residencia a eliminar
+  	residence = Residence.find(params[:id])
+
+  	if residence.destroy
+  		redirect_to residences_path, notice: "La residencia '#{residence.title}' ha sido eliminada con éxito"
+  	else 
+  		redirect_to residences_path, notice: "ERROR al eliminar la residencia '#{residence.title}'" 
+  	end
   end
 
-  def create
 
-    @residence = Residence.new(residence_params)
+  def create
+    @residence = Residence.new(params.require(:residence).permit(:title,:description,:address,:precio,:person))
+    
     if @residence.save
-        redirect_to @residence
+        redirect_to residences_path, notice: 'La residencia fue creado con éxito'
       else
         render :new
       end
+  end
 
-  end
-  def residence_params
-    params.require(:residence).permit(:title,:description,:address,:precio,:person)
-  end
+  #def residence_params
+   # params.require(:residence).permit(:title,:description,:address,:precio,:person)
+  #end
 
 end
