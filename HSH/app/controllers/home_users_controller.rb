fc917e584716876615e71a6018f21f247a033615
Arreche Cristian <arrechecris@gmail.com>
2018-11-13 17:39:43 -0300

    Busqueda de residencias por distintos criterios

--- HSH/app/controllers/home_users_controller.rb
+++ HSH/app/controllers/home_users_controller.rb
@@ -1,12 +1,12 @@
 class HomeUsersController < ApplicationController
 
-
 	def index
+		@residences = params[:com] ? Residence.all.where("complejo LIKE ?", "%#{params[:com]}%") : Residence.all
 
-		@busq = params[:search]
+		@residences = params[:loc] != "" ? @residences.where("ubicacion LIKE ?", "%#{params[:loc]}%") : @residences
 
-		@residences = Residence.all.where("complejo LIKE ?", "%#{params[:search]}%")
+		@precio = params[:pre].to_f
+		@residences = @precio != 0.0 ? @residences.where("(precio >= ?) AND (precio <= ?)", (0.8*@precio).to_i, (1.2*@precio).to_i ): @residences
+
 	end
-
-
 end
