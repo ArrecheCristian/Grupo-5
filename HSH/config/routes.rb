1c3c10d0bc5ca60c53f6823184f0f61fa68ad35a
Arreche Cristian <arrechecris@gmail.com>
2018-11-13 15:17:31 -0300

    Busqueda de residencias por palabras funcionando

--- HSH/config/routes.rb
+++ HSH/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 	
-  get 'home_users/index'
+  
 	resources :residences
- 	resources :clientes
- 	resources :users
- 	resources :home_users
+	resources :clientes
+	resources :users
+	resources :home_users
   root 'users#index'
 
 end
