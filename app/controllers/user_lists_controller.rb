e978e1ab4e3e185fa88a516a79c666f6150e811e
Arreche Cristian <arrechecris@gmail.com>
2018-11-26 21:06:04 -0300

    Cambio de directorio

--- app/controllers/user_lists_controller.rb
+++ app/controllers/user_lists_controller.rb
@@ -8,4 +8,12 @@ class UserListsController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_183949) do
+ActiveRecord::Schema.define(version: 2018_11_27_000012) do
 
   create_table "auctions", force: :cascade do |t|
     t.integer "precioBase"
@@ -67,7 +67,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_183949) do
     t.string "Foto_perfil"
     t.integer "DNI"
     t.boolean "premium"
-    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
