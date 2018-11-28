4cee4911fb188cb4fe4903d4656f545800d0761f
Arreche Cristian <arrechecris@gmail.com>
2018-11-28 02:42:40 -0300

    Arranca?

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
   	devise_for :admins
-  	devise_for :users do
-  		get '/users/sign_out' => 'devise/sessions#destroy'
-	end
+  	devise_for :users
 	resources :residences
  	resources :clientes
  	resources :auctions, :except => 'new'
