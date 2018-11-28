0d26890b03d711d4e1a441efa1e9d912ec0f7e73
Arreche Cristian <arrechecris@gmail.com>
2018-11-28 00:49:22 -0300

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ujs
+//= require jquery_ujs
 //= require jquery-ui
-//= require yourjsfile
-
+//= require turbolinks
+//= require_tree .
