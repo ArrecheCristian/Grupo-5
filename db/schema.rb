c19f43d544a3f3731485b9e8d3a165abd1ecd5f9
ArrecheCristian <arrechecris@gmail.com>
2018-12-05 17:56:16 -0300

    Me olvidé

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-  ActiveRecord::Schema.define(version: 2018_12_02_233406) do
-  
+ActiveRecord::Schema.define(version: 2018_12_03_174701) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
