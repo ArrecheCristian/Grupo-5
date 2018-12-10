4f133d124fc12b3250c0f59382c763df3117919e
ArrecheCristian <arrechecris@gmail.com>
2018-12-10 04:45:23 -0300

    Agregado de scopes para listar correctamente

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, uniqueness: true
 
+  default_scope->{order("Nombre asc")}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
