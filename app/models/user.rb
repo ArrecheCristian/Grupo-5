978b77ee56d182c6070b1b6dbb8d8729a1d104e6
ramiro <ramiro.intas@hotmail.com>
2018-12-10 21:30:24 -0300

    validaciones de registro

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, uniqueness: true
+  validates :Nombre, presence: true
+  validates :Apellido, presence: true
+  validates :Nacimiento, presence: true
+  validates :Numero_tarjeta, presence: true , length: { is: 16 }
+  validates :Vencimiento_tarjeta, presence: true
+  validates :Banco, presence: true
+  validates :DNI, presence: true, length: { in: 8..13 }
+
 
   default_scope->{order("Nombre asc")}
 
