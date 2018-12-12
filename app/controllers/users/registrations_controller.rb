fa8ebb703ffcf7dff685a7c14fc1a7fc07e608e4
ArrecheCristian <arrechecris@gmail.com>
2018-12-12 06:01:29 -0300

    Arreglo sintaxis

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -43,37 +43,38 @@ class Users::RegistrationsController < Devise::RegistrationsController
    end
 
 
-   def update
+    def update
      @user = User.new account_update_params.except("current_password")
      
      difAño = Time.now.year-@user.Nacimiento.year
      difMes =  @user.Nacimiento.month-Time.now.month
      difDia = @user.Nacimiento.day-Time.now.day
 
-     if(difAño>18)
-       super
-     else 
+      if(difAño>18)
+        super
+      else 
         if (difAño == 18) 
-              if (difMes < 0 )
-                    super     
+          if (difMes < 0 )
+            super     
+          else
+            if(difMes == 0) 
+              if(difDia <0 )
+                super
               else
-                 if(difMes == 0) 
-                    if(difDia <0 )
-                      super
-                    else
-                      flash[:alert] = "Fecha no valida, ¿no eres menor de edad o si ?"
+                flash[:alert] = "Fecha no valida, ¿no eres menor de edad o si ?"
                       redirect_to root_path 
-                    end
-                  else
-                    flash[:alert] = "Fecha no valida, ¿no eres menor de edad o si"
+              end
+            else
+              flash[:alert] = "Fecha no valida, ¿no eres menor de edad o si"
                     redirect_to root_path
-                  end
-              end  
+            end
+          end  
         else
               flash[:alert] = "Fecha no valida, ¿no eres menor de edad o si"
               redirect_to root_path
         end
-   end
+      end
+    end
 
 
    def destroy
